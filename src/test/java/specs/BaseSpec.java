58fffdfebbe6aaa132dd925e26c0cf83f1a19647
Margarita Logunova <logunovar@gmail.com>
2026-08-10 23:09:34 +0300

    add clubs

--- src/test/java/specs/BaseSpec.java
+++ src/test/java/specs/BaseSpec.java
@@ -1,6 +1,8 @@
 package specs;
 
+import config.ApiConfig;
 import io.restassured.specification.RequestSpecification;
+import org.aeonbits.owner.ConfigFactory;
 
 import static allure.CustomAllureListener.withCustomTemplate;
 import static io.restassured.RestAssured.with;
@@ -8,9 +10,12 @@ import static io.restassured.http.ContentType.JSON;
 
 public class BaseSpec {
 
+    private static final ApiConfig config = ConfigFactory.create(ApiConfig.class, System.getProperties());
+
     public static RequestSpecification baseRequestSpec = with()
             .filter(withCustomTemplate())
             .log().all()
             .contentType(JSON)
-            .basePath("/api/v1");
+            .baseUri(config.baseUri())
+            .basePath(config.basePath());
 }
