b47ca8894892f613c49f2402c9421ceb089d2fb9
Margarita Logunova <logunovar@gmail.com>
2026-08-18 23:49:45 +0300

    add base API test configuration

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import api.ApiClient;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+import static specs.BaseSpec.baseRequestSpec;
+
+public class BaseTest {
+
+    protected static final ApiClient api = new ApiClient();
+
+    @BeforeAll
+    protected static void configureApi() {
+        RestAssured.requestSpecification = baseRequestSpec;
+    }
+}
